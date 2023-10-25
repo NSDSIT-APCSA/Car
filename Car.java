0cf3a9a2bb8d5ca76a66f5ce432f8c32b82d68bb
Tim Salerno <106488849+tsalerno@users.noreply.github.com>
2023-10-25 11:16:14 -0400

    Update Car.java
    Added boolean hasBlinker, double tirePSI, and method checkTirePressure()

--- Car.java
+++ Car.java
@@ -5,8 +5,11 @@ public class Car {
     //Instance Variables
     private int mileage;
     private int year;
+    private double tirePSI;
     private String brand;
     private String model;
+
+    private boolean hasBlinkers;
 
 
     //Constructors
@@ -21,14 +24,26 @@ public class Car {
         this.year = year;
     }
 
-    public Car(int mileage, int year, String brand, String model) {
+    public Car(int mileage, int year, double psi, String brand, String model) {
         this.mileage = mileage;
         this.year = year;
+        this.tirePSI = psi;
         this.brand = brand;
         this.model = model;
     }
 
-    //Methods
+    // Methods
+
+    public void checkTirePressure(){
+        if (tirePSI > 40) {
+            System.out.println("You need to let some air out!");
+        }
+        if (tirePSI == 36) {
+            System.out.println("Perfect tire pressure!");
+        }
+    }
+    
+    //Getters and Setters
 
     public void setMileage(int mileage) {
         this.mileage = mileage;
@@ -67,13 +82,9 @@ public class Car {
     }
 
     public static void main(String args[]) {
-        Car carOne = new Car();
-        carOne.setMileage(34);
-        carOne.carError();
-        int carOneMiles = carOne.getMileage();
-        System.out.println(carOneMiles);
-        carOne.setMileage(500);
-        System.out.println(carOneMiles);
-        System.out.println(carOne.getMileage());
+        // boolean examples
+        int a = 12;
+        double b = 12.1;
+        System.out.println(a == b);
     }
 }
